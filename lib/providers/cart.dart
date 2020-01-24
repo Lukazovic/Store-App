108c4ea18d6ac33bd03c9cc0d0199bfeb26dd704
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-01-23 22:02:44 -0300

    end of the sction 8

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -28,7 +28,7 @@ class Cart with ChangeNotifier {
   double get totalAmount {
     var total = 0.0;
     _items.forEach((key, cartItem) {
-      total = cartItem.price * cartItem.quantity;
+      total += cartItem.price * cartItem.quantity;
     });
     return total;
   }
@@ -55,6 +55,16 @@ class Cart with ChangeNotifier {
         ),
       );
     }
+    notifyListeners();
+  }
+
+  void removeItem (String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear () {
+    _items = {};
     notifyListeners();
   }
 }
