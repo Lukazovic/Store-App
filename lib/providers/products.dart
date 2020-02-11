51f8e42e48e422c8dffdcc246bfb36a60a0bd31f
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-02-11 01:28:42 -0300

    Update method with web data base

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -118,9 +118,16 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProducts(String id, Product newProduct) {
+  Future<void> updateProducts(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = 'https://store-flutter-app.firebaseio.com/products/$id.json';
+      http.patch(url, body: json.encode({
+        'title': newProduct.title,
+        'description': newProduct.description,
+        'imageUrl': newProduct.imageUrl,
+        'price': newProduct.price,
+      }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     }
