685655c7f085d25abef037d8f7363b943dcaac38
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-02-11 00:05:33 -0300

    Add a error alert dialog

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -103,7 +103,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error ocurred!'),
+            content: Text('Something went wrong!'),
+            actions: <Widget>[
+              FlatButton(child: Text('Okay'), onPressed: () {
+                Navigator.of(ctx).pop();
+              },),
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
